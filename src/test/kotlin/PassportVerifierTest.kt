6618abdc24f754d207232813e0424f5b53fc9273
GideonBrimleaf <chrisdryden1@gmail.com>
2020-12-06 18:07:32 +0000

    can provide map of values

--- src/test/kotlin/PassportVerifierTest.kt
+++ src/test/kotlin/PassportVerifierTest.kt
@@ -4,22 +4,33 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class PassportVerifierTest {
-    private val data = mutableListOf<String>()
-    private var processedData = listOf<String>()
-
+    private var data = mutableListOf<String>()
+    private var mappedData = mutableListOf<Map<String, String>>()
 
     @BeforeTest fun setup() {
         File("./src/main/data/PassportsSample.txt").forEachLine {
-            if(it == "") {
+            if (it == "") {
                 data.add("NEWLINE!!")
             } else {
                 data.add(it)
             }
-            processedData = data.joinToString().split("NEWLINE!!")
         }
+
+        data = data.joinToString().replace(",", "").split(" NEWLINE!! ").toMutableList()
     }
 
     @Test fun `There are four lights`() {
-        assertEquals(4, processedData.size)
+        assertEquals(4, data.size)
+    }
+
+    @Test fun `Can convert to map`() {
+        val splitData = data.map { word -> word.split(" ")}
+
+        splitData.forEach {
+            val mapValue = it.associate { it.substringBefore(":") to it.substringAfter(":") }
+            mappedData.add(mapValue)
+        }
+
+        assertEquals(mapOf("eyr" to "2020"), mappedData.first().filterKeys { it == "eyr" })
     }
 }
